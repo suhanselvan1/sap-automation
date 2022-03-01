b9f1c2ec9fb43e4b229c34c4f5fbb10d2ad0a709
suhanwft <suhans@wftus.com>
2022-03-01 05:13:09 +0000

    Added domain join to OS config DSC

--- deploy/scripts/pwsh/DSCModules/OSConfiguration.ps1
+++ deploy/scripts/pwsh/DSCModules/OSConfiguration.ps1
@@ -1,5 +1,13 @@
 Configuration OSConfiguration
 {
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullorEmpty()]
+        [System.Management.Automation.PSCredential]
+        $Credential
+    )
+    
     Import-DSCResource -ModuleName NetworkingDsc
     Import-DSCResource -ModuleName ComputerManagementDsc
 
@@ -13,24 +21,28 @@ Configuration OSConfiguration
             Name = "NET-Framework-Features"
             Ensure = "Present"
         }
+
         #Disable domain firewall
         FirewallProfile DisableDomainFirewall
         {
             Name                  = 'Domain'
             Enabled                = 'False'
         }
+
         #Disable public firewall
         FirewallProfile DisablePublicFirewall
         {
             Name                  = 'Public'
             Enabled                = 'False'
         }
+
         #Disable private firewall
         FirewallProfile DisablePrivateFirewall
         {
             Name                  = 'Private'
             Enabled                = 'False'
         }
+
         #Set paging file size
         VirtualMemory PagingSettings
         {
@@ -39,9 +51,26 @@ Configuration OSConfiguration
             InitialSize = $VirtualMemorySize
             MaximumSize = $VirtualMemorySize
         }
+
+        #Join domain
+        Computer JoinDomain
+        {
+            Name       = 'Test-AppVM'
+            DomainName = 'automation.com'
+            Credential = $Credential # Credential to join to domain
+        }
     }
 }
 
-OSConfiguration
+$cd = @{
+    AllNodes = @(
+        @{
+            NodeName = 'localhost'
+            PSDscAllowPlainTextPassword = $true
+        }
+    )
+}
+
+OSConfiguration -ConfigurationData $cd
 
 Start-DscConfiguration OSConfiguration -Wait -Verbose
