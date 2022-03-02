901f3aa63d7ad83af4ecfd6266b5d8b01e9530d2
suhanwft <suhans@wftus.com>
2022-03-02 21:11:52 +0000

    Added SQL Install DSC

--- deploy/scripts/pwsh/DSCModules/SQLInstall.ps1
+++ deploy/scripts/pwsh/DSCModules/SQLInstall.ps1
@@ -4,12 +4,14 @@ Configuration SQLInstall
 
      node localhost
      {
+          #Install .Net 4.5	  
           WindowsFeature 'NetFramework45'
           {
                Name   = 'NET-Framework-45-Core'
                Ensure = 'Present'
           }
 
+	  #Install SQL Server
           SqlSetup 'InstallDefaultInstance'
           {
                InstanceName        = 'MSSQLSERVER'
