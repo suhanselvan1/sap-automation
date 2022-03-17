3e58c5e2544f8d49b9ca245cc1f4fbbd9bc83c1f
suhanwft <suhans@wftus.com>
2022-03-17 05:08:02 +0000

    Updated OS configuration module

--- deploy/scripts/pwsh/DSCModules/OSConfiguration.ps1
+++ deploy/scripts/pwsh/DSCModules/OSConfiguration.ps1
@@ -17,12 +17,18 @@ Configuration OSConfiguration
 
         [Parameter(Mandatory = $true)]
         [ValidateNotNullorEmpty()]
-        [string]$DomainName
+        [string]$DomainName,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullorEmpty()]
+        [string]$UsrSapPath
 
     )
     
     Import-DSCResource -ModuleName NetworkingDsc
     Import-DSCResource -ModuleName ComputerManagementDsc
+    Import-DSCResource -ModuleName ComputerManagementDsc
+    Import-DSCResource -ModuleName WindowsDefender
 
     $VirtualMemorySize = [int](((Get-Volume -DriveLetter D).Size/1048576)-2048)
 
@@ -35,25 +41,19 @@ Configuration OSConfiguration
             Ensure = "Present"
         }
 
-        #Disable domain firewall
-        FirewallProfile DisableDomainFirewall
+        # Add inbound firewall rule for SAP ports
+        Firewall AddFirewallRule
         {
-            Name                  = 'Domain'
-            Enabled                = 'False'
-        }
-
-        #Disable public firewall
-        FirewallProfile DisablePublicFirewall
-        {
-            Name                  = 'Public'
-            Enabled                = 'False'
-        }
-
-        #Disable private firewall
-        FirewallProfile DisablePrivateFirewall
-        {
-            Name                  = 'Private'
-            Enabled                = 'False'
+            Name                  = 'SAPFirewallRule'
+            DisplayName           = 'SAP Inbound Firewall Rule'
+            Group                 = 'SAP Firewall Rule Group'
+            Ensure                = 'Present'
+            Enabled               = 'True'
+            Profile               = ('Domain', 'Private','Public')
+            Direction             = 'Inbound'
+            LocalPort             = ('3200-3299','8000-8099','44300-44399','30000-30099','50000-50020','1128','1129','4237','4239','3300-3399','3600-3699','15','30013-30049','50200-50205','8100-8105','44300-44305','8000-8005','50010-50015','30010-30015','8010-8015','50110-50115','30100-30115','443','3300-3310','3600-3610','3900-3910')
+            Protocol              = 'TCP'
+            Description           = 'SAP Inbound Firewall Rule'
         }
 
         #Set paging file size
@@ -72,6 +72,13 @@ Configuration OSConfiguration
             DomainName = $DomainName
             Credential = $DomainCredential # Credential to join to domain
         }
+
+        # Configure Windows Deefender
+        WindowsDefender DefenderExclusion
+        {
+        IsSingleInstance = 'yes'
+        ExclusionPath = ("$UsrSapPath")
+        }
     }
 }
 
@@ -86,4 +93,4 @@ $cd = @{
 
 OSConfiguration -ConfigurationData $cd
 
-Start-DscConfiguration OSConfiguration -Wait -Verbose
+Start-DscConfiguration OSConfiguration -Wait -Verbose -Force
