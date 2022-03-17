fb25e4f496eaaafb317ed1ea3fd167fbeebd5863
suhanwft <suhans@wftus.com>
2022-03-17 06:04:52 +0000

    Updated SAP install module

--- deploy/scripts/pwsh/DSCModules/SAPInstall.ps1
+++ deploy/scripts/pwsh/DSCModules/SAPInstall.ps1
@@ -1,7 +1,4 @@
-Configuration SAPInstall
-{
-
-    param
+param
     (
         [Parameter(Mandatory = $true)]
         [ValidateNotNullorEmpty()]
@@ -9,24 +6,13 @@ Configuration SAPInstall
 
         [Parameter(Mandatory = $true)]
         [ValidateNotNullorEmpty()]
-        [string]$ParameterFilePath
+        [string]$ParameterFilePath,
+
+	[Parameter(Mandatory = $true)]
+        [ValidateNotNullorEmpty()]
+        [string]$ProductID
     )
 
-    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+$InstallCommand = "$MediaPath SAPINST_INPUT_PARAMETERS_URL=$ParameterFilePath SAPINST_EXECUTE_PRODUCT_ID=$ProductID SAPINST_SKIP_DIALOGS=true SAPINST_START_GUI=false SAPINST_START_GUISERVER=false"
 
-    $InstallCommand = "$MediaPath SAPINST_INPUT_PARAMETERS_URL=$ParameterFilePath SAPINST_EXECUTE_PRODUCT_ID=NW_ABAP_CI:NW750.MSS.ABAP SAPINST_SKIP_DIALOGS=true SAPINST_START_GUI=false SAPINST_START_GUISERVER=false"
-
-    Node localhost
-    {
-        Script ScriptExample
-        {
-            SetScript = { $InstallCommand }
-            TestScript = { return $False }
-            GetScript = { return $True }
-        }
-    }
-}
-
-SAPInstall
-
-Start-DscConfiguration SAPInstall -Wait -Verbose -Force
+Invoke-Expression -Command $InstallCommand
