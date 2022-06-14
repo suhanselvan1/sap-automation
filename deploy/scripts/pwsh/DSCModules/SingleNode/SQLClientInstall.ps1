f358e3ab6769f50c420da7fb241cdd7dbb204e87
suhan selvan <sselvan@lemongrasscloud.com>
2022-06-14 01:11:28 +0000

    Added HA configuration files

--- deploy/scripts/pwsh/DSCModules/SingleNode/SQLClientInstall.ps1
+++
@@ -1,9 +0,0 @@
-param
-    (
-	[Parameter(Mandatory = $true)]
-        [ValidateNotNullorEmpty()]
-        [string]$BinPath
-    )
-
-$SQLClientInstall = "MSIEXEC.EXE /I $BinPath\msodbcsql.msi /qb IACCEPTMSODBCSQLLICENSETERMS=YES"
-Invoke-Expression -Command $SQLClientInstall
